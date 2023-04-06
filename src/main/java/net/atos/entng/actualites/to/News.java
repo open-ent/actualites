e80ed3c5e3cc8a96e890a198c0088e9dfd3c5ccf
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-04-06 14:17:06 +0200

    feat: WB3-3 empty request for info paged list by thread

---
+++ src/main/java/net/atos/entng/actualites/to/News.java
@@ -0,0 +1,13 @@
+package net.atos.entng.actualites.to;
+
+public class News {
+    private String title;
+
+    public News(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
