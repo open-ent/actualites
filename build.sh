112c07fb8376a3e73a5de15bd0549e34bd6739da
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2025-08-05 09:28:13 +0200

    step 2.2

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Frontend
+cd frontend
+./build.sh --no-docker clean init build
+cd ..
+
+# Create directory structure and copy frontend dist
+cd backend
+find ./src/main/resources/public/ -maxdepth 1 -type f -exec rm -f {} +
+cp -R ../frontend/dist/* ./src/main/resources/
+
+# Copy Files
+mv ./src/main/resources/*.html ./src/main/resources/view
+
+# Build .
+./build.sh --no-docker clean build
+
+# Clean up - remove frontend/dist and backend/src/main/resources
+rm -rf ../frontend/dist
