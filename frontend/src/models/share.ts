cd961e3b362467201cc72fb775bdee26749e7e5b
jcbe-ode <67910608+jcbe-ode@users.noreply.github.com>
2025-08-29 09:45:12 +0200

    feat: COCO-4380, initialisation frontend des services (#45)
    * feat: COCO-4380, thread service
    
    * continued
    
    * feat: COCO-4380, info service
    
    * chore: update pnpm-lock.yaml
    
    * mocked data
    
    * add contentVersion
    
    * add originalFormat support
    
    * add infos hooks and tests
    
    * add comments service, hooks and tests
    
    * feedback from PR
    
    * remove unnecessary staleTime values

---
+++ frontend/src/models/share.ts
@@ -0,0 +1,27 @@
+export interface Share {
+  actions: Array<{
+    name: Array<string>;
+    displayName: string;
+    type: 'RESOURCE';
+  }>;
+  groups: {
+    visibles: Array<{
+      id: string;
+      name: string;
+      groupDisplayName: null | string;
+      structureName: string;
+    }>;
+    checked: { [right: string]: Array<string> };
+  };
+  users: {
+    visibles: Array<{
+      id: string;
+      login: string;
+      username: string;
+      lastName: string;
+      firstName: string;
+      profile: string;
+    }>;
+    checked: { [right: string]: Array<string> };
+  };
+}
---
+++ frontend/src/services/queries/info.test.tsx
@@ -0,0 +1,203 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { act } from 'react';
+import { wrapper } from '~/mocks/setup';
+import { InfoStatus } from '~/models/info';
+import { infoService } from '../api';
+import {
+  useCreateDraftInfo,
+  useDeleteInfo,
+  useInfos,
+  useInfoShares,
+  usePublishInfo,
+  useSubmitInfo,
+  useUnsubmitInfo,
+  useUpdateInfo,
+} from './info';
+
+describe('Info Queries', () => {
+  test('useInfos hook to get a page of infos', async () => {
+    const serviceSpy = vi.spyOn(infoService, 'getInfos');
+
+    renderHook(() => useInfos(0).data, {
+      wrapper,
+    });
+
+    await waitFor(() => {
+      expect(serviceSpy).toHaveBeenCalled();
+    });
+  });
+
+  test('useCreateDraftInfo hook to create an info', async () => {
+    const { result } = renderHook(() => useCreateDraftInfo(), {
+      wrapper,
+    });
+
+    const serviceSpy = vi.spyOn(infoService, 'createDraft');
+
+    const variables = {
+      title: 'Création de fil',
+      content: '<div>New draft</div>',
+      thread_id: 1,
+    };
+
+    act(() => {
+      result.current.mutate(variables);
+    });
+
+    await waitFor(() => {
+      expect(serviceSpy).toHaveBeenCalledWith(variables);
+    });
+  });
+
+  test('useUpdateInfo hook to update an info', async () => {
+    const { result } = renderHook(() => useUpdateInfo(), {
+      wrapper,
+    });
+
+    const serviceSpy = vi.spyOn(infoService, 'update');
+
+    const variables = {
+      threadId: 1,
+      infoId: 123,
+      infoStatus: InfoStatus.PUBLISHED,
+      payload: {
+        title: 'Changement de titre',
+        content: 'Changement de contenu',
+        is_headline: true,
+      },
+    };
+
+    act(() => {
+      result.current.mutate(variables);
+    });
+
+    await waitFor(() => {
+      expect(serviceSpy).toHaveBeenCalledWith(
+        variables.threadId,
+        variables.infoId,
+        variables.infoStatus,
+        variables.payload,
+      );
+    });
+  });
+
+  test('useDeleteInfo hook to delete an info', async () => {
+    const { result } = renderHook(() => useDeleteInfo(), {
+      wrapper,
+    });
+
+    const serviceSpy = vi.spyOn(infoService, 'delete');
+
+    const variables = { threadId: 1, infoId: 123 };
+
+    act(() => {
+      result.current.mutate(variables);
+    });
+
+    await waitFor(() => {
+      expect(serviceSpy).toHaveBeenCalledWith(
+        variables.threadId,
+        variables.infoId,
+      );
+    });
+  });
+
+  test('useInfoShares hook to get info shares', async () => {
+    const serviceSpy = vi.spyOn(infoService, 'getShares');
+
+    const variables = { threadId: 1, infoId: 123 };
+
+    renderHook(() => useInfoShares(variables.threadId, variables.infoId).data, {
+      wrapper,
+    });
+
+    await waitFor(() => {
+      expect(serviceSpy).toHaveBeenCalledWith(
+        variables.threadId,
+        variables.infoId,
+      );
+    });
+  });
+
+  test('useSubmitInfo hook to submit an info', async () => {
+    const { result } = renderHook(() => useSubmitInfo(), {
+      wrapper,
+    });
+
+    const serviceSpy = vi.spyOn(infoService, 'submit');
+
+    const variables = {
+      threadId: 1,
+      infoId: 123,
+      payload: {
+        title: 'Changement de titre',
+      },
+    };
+
+    act(() => {
+      result.current.mutate(variables);
+    });
+
+    await waitFor(() => {
+      expect(serviceSpy).toHaveBeenCalledWith(
+        variables.threadId,
+        variables.infoId,
+        variables.payload,
+      );
+    });
+  });
+
+  test('useUnsubmitInfo hook to unsubmit an info', async () => {
+    const { result } = renderHook(() => useUnsubmitInfo(), {
+      wrapper,
+    });
+
+    const serviceSpy = vi.spyOn(infoService, 'unsubmit');
+
+    const variables = {
+      threadId: 1,
+      infoId: 123,
+    };
+
+    act(() => {
+      result.current.mutate(variables);
+    });
+
+    await waitFor(() => {
+      expect(serviceSpy).toHaveBeenCalledWith(
+        variables.threadId,
+        variables.infoId,
+      );
+    });
+  });
+
+  test('usePublishInfo hook to submit an info', async () => {
+    const { result } = renderHook(() => usePublishInfo(), {
+      wrapper,
+    });
+
+    const serviceSpy = vi.spyOn(infoService, 'publish');
+
+    const variables = {
+      threadId: 1,
+      infoId: 123,
+      payload: {
+        title: 'Changement de titre',
+        owner: 'owner',
+        username: 'username',
+      },
+    };
+
+    act(() => {
+      result.current.mutate(variables);
+    });
+
+    await waitFor(() => {
+      expect(serviceSpy).toHaveBeenCalledWith(
+        variables.threadId,
+        variables.infoId,
+        variables.payload,
+      );
+    });
+  });
+});
