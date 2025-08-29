42b6fb99e652e72decf570bb60d59088b037cd73
jcbe-ode <67910608+jcbe-ode@users.noreply.github.com>
2025-08-29 09:45:12 +0200

    feat: COCO-4380, initialisation frontend des services (#45)
    * feat: COCO-4380, thread service
    
    * continued
    
    * feat: COCO-4380, info service
    
    * chore: update pnpm-lock.yaml
    
    * mocked data
    
    * add contentVersion
    
    * add originalFormat support
    
    * add infos hooks and tests
    
    * add comments service, hooks and tests
    
    * feedback from PR
    
    * remove unnecessary staleTime values

---
+++ frontend/src/models/info.ts
@@ -0,0 +1,85 @@
+import { ThreadId } from './thread';
+
+export type InfoId = number;
+
+export type InfoOwner = {
+  id: string;
+  displayName: string;
+  deleted?: boolean;
+};
+
+export enum InfoStatus {
+  TRASH = 'TRASH', // 0
+  DRAFT = 'DRAFT', // 1
+  PENDING = 'PENDING', // 2
+  PUBLISHED = 'PUBLISHED', // 3
+}
+
+export interface Info {
+  id: InfoId;
+  threadId: ThreadId;
+  content: string;
+  status: InfoStatus;
+  owner: InfoOwner;
+  /** Creation date, formatted as 'YYYY-MM-DDTHH:mm:ss' */
+  created: string;
+  /** Modification date, formatted as 'YYYY-MM-DDTHH:mm:ss' */
+  modified: string;
+  /** Publication date, formatted as 'YYYY-MM-DDTHH:mm:ss' */
+  publicationDate: string | null;
+  /** Expiration date, formatted as 'YYYY-MM-DDTHH:mm:ss' */
+  expirationDate: string | null;
+  numberOfComments: number;
+  title: string;
+  headline: boolean;
+  sharedRights: [];
+}
+
+export enum InfoRevisionEvent {
+  DRAFT = 'DRAFT',
+  PENDING = 'PENDING',
+  SUBMIT = 'SUBMIT',
+  CREATE_AND_PENDING = 'CREATE_AND_PENDING',
+  CREATE_AND_PUBLISH = 'CREATE_AND_PUBLISH',
+  UPDATE = 'UPDATE',
+  PUBLISH = 'PUBLISH',
+  UNPUBLISH = 'UNPUBLISH',
+}
+
+export interface InfoRevision {
+  _id: number;
+  created: string;
+  title: string;
+  content: string;
+  contentVersion: 0 | 1;
+  user_id: string;
+  eventname: InfoRevisionEvent;
+  username: string;
+}
+
+export interface OriginalInfo {
+  _id: number;
+  title: string;
+  content: string;
+  contentVersion: 0 | 1;
+  status: 0 | 1 | 2 | 3; // see InfoStatus enum
+  publication_date: string | null;
+  expiration_date: string | null;
+  is_headline: boolean;
+  thread_id: ThreadId;
+  created: string;
+  modified: string;
+  owner: string;
+  username: string;
+  thread_title: string;
+  thread_icon: string;
+  comments: Array<{
+    _id: InfoId;
+    comment: string;
+    owner: string;
+    created: string;
+    modified: string;
+    username: string;
+  }> | null;
+  shared: [];
+}
---
+++ frontend/src/services/queries/thread.ts
@@ -0,0 +1,76 @@
+import { queryOptions, useMutation, useQuery } from '@tanstack/react-query';
+import { ThreadId, ThreadMode } from '~/models/thread';
+import { threadService } from '../api';
+
+export const threadQueryKeys = {
+  all: () => ['threads'] as const,
+  thread: (threadId?: ThreadId) => [...threadQueryKeys.all(), threadId],
+  share: (threadId: ThreadId) => [
+    ...threadQueryKeys.thread(threadId),
+    'share',
+    'json',
+  ],
+};
+
+/**
+ * Provides query options for thread-related operations.
+ */
+export const threadQueryOptions = {
+  /**
+   * @returns Query options for fetching the threads.
+   */
+  getThreads() {
+    return queryOptions({
+      queryKey: threadQueryKeys.all(),
+      queryFn: () => threadService.getThreads(),
+    });
+  },
+
+  /**
+   * Retrieves the share rights on a thread.
+   *
+   * @param threadId - The ID of the thread.
+   * @returns Query options for fetching the share rights.
+   */
+  getShares(threadId: ThreadId) {
+    return queryOptions({
+      queryKey: threadQueryKeys.share(threadId),
+      queryFn: () => threadService.getShares(threadId),
+    });
+  },
+};
+
+export const useThreads = () => useQuery(threadQueryOptions.getThreads());
+export const useThreadShares = (threadId: ThreadId) =>
+  useQuery(threadQueryOptions.getShares(threadId));
+
+export const useCreateThread = () =>
+  useMutation({
+    mutationFn: (payload: { mode: ThreadMode; title: string }) =>
+      threadService.create(payload),
+    // TODO optimistic update
+    // onSuccess: async (, { mode, title }) => {
+    // },
+  });
+
+export const useUpdateThread = () =>
+  useMutation({
+    mutationFn: ({
+      threadId,
+      payload,
+    }: {
+      threadId: ThreadId;
+      payload: { mode: ThreadMode; title: string };
+    }) => threadService.update(threadId, payload),
+    // TODO optimistic update
+    // onSuccess: async (, { mode, title }) => {
+    // },
+  });
+
+export const useDeleteThread = () =>
+  useMutation({
+    mutationFn: (threadId: ThreadId) => threadService.delete(threadId),
+    // TODO optimistic update
+    // onSuccess: async (, { mode, title }) => {
+    // },
+  });
