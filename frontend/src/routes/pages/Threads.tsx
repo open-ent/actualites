8de43be5576525db6c9012a2489018046033684a
RomuC <romuald.coulaud@gmail.com>
2025-09-02 10:14:01 +0200

    feat: #COCO-4536 init layout

--- frontend/src/routes/pages/Threads.tsx
+++ frontend/src/routes/pages/Threads.tsx
@@ -1,20 +1,15 @@
 import { QueryClient } from '@tanstack/react-query';
 import { useParams, useSearchParams } from 'react-router-dom';
+import { InfoList } from '~/components';
 
 export const loader = (queryClient: QueryClient) => async () => {
   return null;
 };
 
 export function Threads() {
-  const { threadId, info } = useParams();
+  const { threadId } = useParams();
   const [searchParams] = useSearchParams();
   const status = searchParams.get('status');
 
-  return (
-    <div>
-      Threads {threadId && <span>with thread ID {threadId}</span>}
-      {info && <span> and info {info}</span>}
-      {status && <span> and status {status}</span>}
-    </div>
-  );
+  return <InfoList />;
 }
