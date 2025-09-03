82c55a80399589aa2950136b478e115282012543
jcbe-ode <67910608+jcbe-ode@users.noreply.github.com>
2025-09-03 09:24:20 +0200

    feat: COCO-4498, SeparatedInfo component (#50)
    * feat: COCO-4498, SeparatedInfo component
    
    * feedback from PR

--- frontend/src/components/InfoCard.tsx
+++ frontend/src/components/InfoCard.tsx
@@ -1,15 +1,22 @@
+import { useDate } from '@edifice.io/react';
 import { Info } from '~/models/info';
+import { SeparatedInfo } from './SeparatedInfo';
 
 export type InfoCardProps = {
   info: Info;
 };
 
 export const InfoCard = ({ info }: InfoCardProps) => {
+  const { formatDate } = useDate();
+
   return (
     <article className="mb-16">
       <header>
         <h1>{info.title}</h1>
-        <div>créé le : {info.created}</div>
+        <SeparatedInfo>
+          <div>{info.owner.displayName}</div>
+          <div>{formatDate(info.modified, 'long')}</div>
+        </SeparatedInfo>
       </header>
       <section>
         <p>{info.content}</p>
