a2715646e90a452c4e506febc2c2bfc5c6cfdef6
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2025-09-04 17:46:16 +0200

    wip

---
+++ frontend/src/components/InfoCard/InfoCardFooter.tsx
@@ -0,0 +1,8 @@
+import { Flex } from '@edifice.io/react';
+import { InfoCardProps } from './InfoCard';
+
+export const InfoCardFooter = ({ info }: Pick<InfoCardProps, 'info'>) => {
+  <footer>
+    <Flex align="center" justify="between"></Flex>
+  </footer>;
+};
