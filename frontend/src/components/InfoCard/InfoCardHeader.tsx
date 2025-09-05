38d1496c1260ec3afbd1308eefa502abe088d766
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2025-09-05 08:31:43 +0200

    wip

--- frontend/src/components/InfoCard/InfoCardHeader.tsx
+++ frontend/src/components/InfoCard/InfoCardHeader.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Flex, useDate } from '@edifice.io/react';
+import { Avatar, Flex, useDate, useDirectory } from '@edifice.io/react';
 import { useThread } from '~/features/threads/useThread';
 import { SeparatedInfo } from '../SeparatedInfo';
 import { ThreadCard } from '../ThreadCard';
@@ -7,19 +7,27 @@ import { InfoCardProps } from './InfoCard';
 export const InfoCardHeader = ({ info }: Pick<InfoCardProps, 'info'>) => {
   const { formatDate } = useDate();
   const thread = useThread(info.threadId);
+  const { getAvatarURL } = useDirectory();
+  const avatarUrl = getAvatarURL(info.owner.id, 'user');
 
   return (
     <header>
       <Flex align="center" justify="between">
-        <ThreadCard thread={thread}></ThreadCard>
-        <h1>{info.title}</h1>
+        <ThreadCard thread={thread} textGray="700"></ThreadCard>
+        <h2>{info.title}</h2>
         <div>{/*TODO : flag *Nouveau */}</div>
       </Flex>
 
       <Flex align="center" gap="16" justify="around">
         <hr className="m-12 flex-fill" />
         <Flex gap="12" align="center" justify="around">
-          <Avatar alt={info.owner.displayName} size="md" variant="circle" />
+          <Avatar
+            alt={info.owner.displayName}
+            src={avatarUrl}
+            size="md"
+            variant="circle"
+            loading="lazy"
+          />
           <SeparatedInfo>
             <div>{info.owner.displayName}</div>
             <div>{formatDate(info.modified, 'long')}</div>
