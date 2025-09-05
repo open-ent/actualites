43443f5b7f023984eda7fabfb1252fb1e8d96a41
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2025-09-05 08:31:43 +0200

    wip

--- frontend/src/components/InfoCard/InfoCardFooter.tsx
+++ frontend/src/components/InfoCard/InfoCardFooter.tsx
@@ -2,7 +2,9 @@ import { Flex } from '@edifice.io/react';
 import { InfoCardProps } from './InfoCard';
 
 export const InfoCardFooter = ({ info }: Pick<InfoCardProps, 'info'>) => {
-  <footer>
-    <Flex align="center" justify="between"></Flex>
-  </footer>;
+  return (
+    <footer>
+      <Flex align="center" justify="between"></Flex>
+    </footer>
+  );
 };
--- frontend/src/components/ThreadCard.tsx
+++ frontend/src/components/ThreadCard.tsx
@@ -1,8 +1,15 @@
 import { AppIcon, Flex, Image, useEdificeClient } from '@edifice.io/react';
+import clsx from 'clsx';
 import { useMemo } from 'react';
 import { Thread } from '~/models/thread';
 
-export const ThreadCard = ({ thread }: { thread?: Thread }) => {
+export const ThreadCard = ({
+  thread,
+  textGray = '800',
+}: {
+  thread?: Thread;
+  textGray?: '700' | '800';
+}) => {
   const { currentApp } = useEdificeClient();
 
   const { iconElement, title } = useMemo(
@@ -14,19 +21,25 @@ export const ThreadCard = ({ thread }: { thread?: Thread }) => {
           width={32}
           height={32}
           objectFit="contain"
+          loading="lazy"
         />
       ) : (
-        <AppIcon app={currentApp}></AppIcon>
+        <AppIcon app={currentApp} variant="square" size="32"></AppIcon>
       ),
       title: thread?.title ?? '',
     }),
     [thread],
   );
 
+  const titleClass = clsx(
+    'fs-3 lh-sm',
+    textGray ? `text-gray-${textGray}` : '',
+  );
+
   return (
     <Flex direction="row" align="center" gap="8">
       {iconElement}
-      <span className="text-gray-700">{title}</span>
+      <span className={titleClass}>{title}</span>
     </Flex>
   );
 };
