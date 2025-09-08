646e314ff796aa3c6162311e72365f9ef6942a90
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2025-09-08 11:43:00 +0200

    add 1 translation and TODOs

--- frontend/src/components/InfoCard/InfoCardFooter.tsx
+++ frontend/src/components/InfoCard/InfoCardFooter.tsx
@@ -1,16 +1,18 @@
 import { Button, Flex } from '@edifice.io/react';
 import { IconRafterDown } from '@edifice.io/react/icons';
+import { useTranslation } from 'react-i18next';
 import { SeparatedInfo } from '../SeparatedInfo';
 import { InfoCardProps } from './InfoCard';
 
 export const InfoCardFooter = ({ info }: Pick<InfoCardProps, 'info'>) => {
+  const { t } = useTranslation();
   const icon = <IconRafterDown></IconRafterDown>;
   return (
     <footer className="mt-12">
       <Flex align="center" justify="between">
         <SeparatedInfo>
-          <span>20 👁️</span>
-          <span>3 💬</span>
+          <span>{/*TODO : nombre de vues */}</span>
+          <span>{/*TODO : nombre de commentaires */}</span>
         </SeparatedInfo>
         <Button
           type="button"
@@ -20,7 +22,7 @@ export const InfoCardFooter = ({ info }: Pick<InfoCardProps, 'info'>) => {
           rightIcon={icon}
           className="btn-icon"
         >
-          Lire la suite
+          {t('actualites.read.more')}
         </Button>
       </Flex>
     </footer>
