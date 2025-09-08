d9d9c39511a5d1b50734dc5ea5f98fe2b28cc905
jcbe-ode <67910608+jcbe-ode@users.noreply.github.com>
2025-09-08 11:21:39 +0200

    feat: COCO-4499, Divider component (#54)
    * feat: COCO-4499, Divider component
    
    * remove unnecessary css file

--- frontend/src/components/InfoCard/InfoCardHeader.tsx
+++ frontend/src/components/InfoCard/InfoCardHeader.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Flex, useDate, useDirectory } from '@edifice.io/react';
 import { useThread } from '~/features/threads/useThread';
+import { Divider } from '../Divider';
 import { SeparatedInfo } from '../SeparatedInfo';
 import { ThreadCard } from '../ThreadCard';
 import { InfoCardProps } from './InfoCard';
@@ -15,26 +16,22 @@ export const InfoCardHeader = ({ info }: Pick<InfoCardProps, 'info'>) => {
       <Flex align="center" justify="between">
         <ThreadCard thread={thread} textGray="700"></ThreadCard>
         <h2>{info.title}</h2>
-        <div>Nouveau{/*TODO : flag Nouveau */}</div>
+        <div>{/*TODO : flag Nouveau */}</div>
       </Flex>
 
-      <Flex align="center" gap="16" justify="around">
-        <hr className="m-12 ms-0 flex-fill" />
-        <Flex gap="12" align="center" justify="around">
-          <Avatar
-            alt={info.owner.displayName}
-            src={avatarUrl}
-            size="md"
-            variant="circle"
-            loading="lazy"
-          />
-          <SeparatedInfo>
-            <div>{info.owner.displayName}</div>
-            <div>{formatDate(info.modified, 'long')}</div>
-          </SeparatedInfo>
-        </Flex>
-        <hr className="m-12 me-0 flex-fill" />
-      </Flex>
+      <Divider color="red">
+        <Avatar
+          alt={info.owner.displayName}
+          src={avatarUrl}
+          size="md"
+          variant="circle"
+          loading="lazy"
+        />
+        <SeparatedInfo>
+          <div>{info.owner.displayName}</div>
+          <div>{formatDate(info.modified, 'long')}</div>
+        </SeparatedInfo>
+      </Divider>
     </header>
   );
 };
