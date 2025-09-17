5bda53eaba4a7f8411da827b60a81be723e1cbdd
RomuC <romuald.coulaud@gmail.com>
2025-09-17 11:45:48 +0200

    Add husky pre push

--- frontend/src/routes/pages/Threads.tsx
+++ frontend/src/routes/pages/Threads.tsx
@@ -2,14 +2,14 @@ import { QueryClient } from '@tanstack/react-query';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { InfoList } from '~/components';
 
-export const loader = (queryClient: QueryClient) => async () => {
+export const loader = (_queryClient: QueryClient) => async () => {
   return null;
 };
 
 export function Threads() {
-  const { threadId } = useParams();
-  const [searchParams] = useSearchParams();
-  const status = searchParams.get('status');
+  const { threadId: _threadId } = useParams();
+  const [_searchParams] = useSearchParams();
+  // const status = searchParams.get('status');
 
   return <InfoList />;
 }
