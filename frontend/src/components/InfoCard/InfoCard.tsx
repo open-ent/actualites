9528242ee1e05207b2a603e6b2126f81811c8509
RomuC <romuald.coulaud@gmail.com>
2025-09-22 10:52:50 +0200

    feat: #COCO-4415 info card animation + state color

--- frontend/src/components/InfoCard/InfoCard.tsx
+++ frontend/src/components/InfoCard/InfoCard.tsx
@@ -1,5 +1,9 @@
+import { Alert, Card } from '@edifice.io/react';
 import clsx from 'clsx';
-import { Info } from '~/models/info';
+import { useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Info, InfoExtendedStatus, InfoStatus } from '~/models/info';
+import './InfoCard.css';
 import { InfoCardContent } from './InfoCardContent';
 import { InfoCardFooter } from './InfoCardFooter';
 import { InfoCardHeader } from './InfoCardHeader';
@@ -12,16 +16,73 @@ export type InfoCardProps = {
 };
 
 export const InfoCard = ({ info }: InfoCardProps) => {
+  const { t } = useTranslation('actualites');
+  const cardRef = useRef<HTMLDivElement>(null);
+
   const infoId = `info-${info.id}`;
-  const className = clsx('mb-16 px-24 py-16');
+  const isIncoming =
+    info.status === InfoStatus.PUBLISHED &&
+    !!info.publicationDate &&
+    new Date(info.publicationDate) > new Date();
+  const isExpired =
+    info.status === InfoStatus.PUBLISHED &&
+    !!info.expirationDate &&
+    new Date(info.expirationDate) < new Date();
+  const [collapse, setCollapse] = useState(true);
+  const className = clsx(
+    'mb-16 px-24 py-16 info-card position-relative border-none overflow-visible',
+    {
+      'info-card-incoming': isIncoming,
+      'info-card-draft': info.status === InfoStatus.DRAFT,
+      'info-card-pending': info.status === InfoStatus.PENDING,
+      'info-card-headline': info.headline,
+      'info-card-expired': isExpired,
+      'info-card-full-content': !collapse,
+    },
+  );
+
+  let extendedStatus: InfoExtendedStatus | undefined;
+  if (isIncoming) {
+    extendedStatus = InfoExtendedStatus.INCOMING;
+  } else if (isExpired) {
+    extendedStatus = InfoExtendedStatus.EXPIRED;
+  } else {
+    extendedStatus = undefined;
+  }
+
+  const handleMoreClick = () => {
+    setCollapse((collapse) => {
+      if (!collapse && cardRef.current) {
+        // Scroll to top of the card when collapsing
+        cardRef.current.scrollIntoView({ behavior: 'smooth' });
+      }
+      return !collapse;
+    });
+  };
 
   return (
-    <article id={infoId} className={className}>
-      <InfoCardHeader info={info} />
+    <Card
+      ref={cardRef}
+      className={className}
+      isClickable={false}
+      isSelectable={false}
+    >
+      <article id={infoId} className="overflow-hidden">
+        <InfoCardHeader info={info} extendedStatus={extendedStatus} />
 
-      <InfoCardContent info={info} />
+        {isExpired && (
+          <Alert type="danger" className="mb-16">
+            <div>
+              <strong>{t('info.alert.expired.title')}</strong>
+            </div>
+            <div>{t('actualites.info.status.expired.description')}</div>
+          </Alert>
+        )}
 
-      <InfoCardFooter info={info} />
-    </article>
+        <InfoCardContent info={info} collapse={collapse} />
+
+        <InfoCardFooter info={info} onMoreClick={handleMoreClick} />
+      </article>
+    </Card>
   );
 };
