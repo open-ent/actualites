c02a55a69182397cf9afa5953738dd16d49f7843
RomuC <romuald.coulaud@gmail.com>
2025-10-07 12:08:09 +0200

    fix form validation

--- frontend/src/features/createInfo/CreateInfoForm.tsx
+++ frontend/src/features/createInfo/CreateInfoForm.tsx
@@ -46,7 +46,7 @@ export function CreateInfoForm() {
     register,
     control,
     setValue,
-
+    trigger,
     formState: { isValid },
     getValues,
   } = useForm<InfoParams>({
@@ -69,6 +69,7 @@ export function CreateInfoForm() {
 
   const handleEditorChange = ({ editor }: { editor: EditorInstance }) => {
     setValue('content', editor.isEmpty ? '' : editor.getHTML());
+    trigger('content');
   };
 
   const handleSubmit = () => {
@@ -135,10 +136,10 @@ export function CreateInfoForm() {
       <FormControl id={'content'} className="mt-24" isRequired>
         <Label>{t('actualites.info.createForm.contentLabel')}</Label>
         <Controller
-          name="threadId"
+          name="content"
           control={control}
           rules={{ required: true }}
-          render={({}) => (
+          render={() => (
             <Flex wrap="nowrap" className="create-info-form_content">
               <Editor
                 content={''}
