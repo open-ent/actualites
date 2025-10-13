5c4b5abdb7ed816c55571ab8b15f46dc54fbdfa9
RomuC <romuald.coulaud@gmail.com>
2025-10-13 11:31:58 +0200

    Fix nullable assertion

--- frontend/src/features/threads/useThread.ts
+++ frontend/src/features/threads/useThread.ts
@@ -2,10 +2,11 @@ import { useMemo } from 'react';
 import { ThreadId } from '~/models/thread';
 import { useThreads } from '~/services/queries/thread';
 
-export function useThread(threadId: ThreadId) {
+export function useThread(threadId?: ThreadId) {
   const threads = useThreads();
 
   const thread = useMemo(() => {
+    if (!threadId) return undefined;
     return threads?.data?.find((thread) => thread._id === threadId);
   }, [threads.data, threadId]);
 
