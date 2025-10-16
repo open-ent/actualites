313b67bb8a9cb01127f15c560dc7dd4de5af8d41
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-10-16 09:38:29 +0200

    feat(actualites):#COCO-4802-mock-i18n (#76)
    * feat(actualites):#COCO-4802-mock-i18n
    
    * feat(actualites):#COCO-4802-mock-i18n

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,8 +1,10 @@
 import { commentHandlers } from './handlers/comment-handlers';
+import { i18nHandlers } from './handlers/i18n-handlers';
 import { infoHandlers } from './handlers/info-handlers';
 import { threadHandlers } from './handlers/thread-handlers';
 
 export const handlers = [
+  ...i18nHandlers,
   ...threadHandlers,
   ...infoHandlers,
   ...commentHandlers,
