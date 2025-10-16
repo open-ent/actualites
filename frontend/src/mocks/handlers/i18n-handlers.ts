6eaded4f61ee49bb82ce0b1b5e9911b5f5e731da
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-10-16 16:35:06 +0200

    feat(actualites):#COCO-4802-mock-i18n fix build excluding mocks i18n for ts compiler

--- frontend/src/mocks/handlers/i18n-handlers.ts
+++ frontend/src/mocks/handlers/i18n-handlers.ts
@@ -1,5 +1,8 @@
 import { http, HttpResponse } from 'msw';
+// @ts-ignore - Backend translation files only use for mock server
 import actualitesTranslations from '../../../../backend/src/main/resources/i18n/fr.json';
+// @ts-ignore - Backend translation files only use for mock server
+// the "actualites" folder have to be next to the "entcore" folder to get the correct path
 import portalTranslations from '../../../../../entcore/portal/backend/src/main/resources/i18n/fr.json';
 
 export const i18nHandlers = [
