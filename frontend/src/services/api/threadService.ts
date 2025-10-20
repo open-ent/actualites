0a5b154241e71aafdab45c1e4e65f695520e096e
RomuC <romuald.coulaud@gmail.com>
2025-10-20 12:11:06 +0200

    chore: use API v1

--- frontend/src/services/api/threadService.ts
+++ frontend/src/services/api/threadService.ts
@@ -1,8 +1,18 @@
 import { odeServices } from '@edifice.io/client';
-import { baseUrl } from '.';
+import { baseUrlAPI } from '.';
 import { Share } from '../../models/share';
 import { Thread, ThreadId, ThreadMode } from '../../models/thread';
 
+/**
+ * Creates a thread service that provides methods for managing threads.
+ *
+ * @returns An object containing methods to:
+ * - Get all threads
+ * - Create a new thread with mode and title
+ * - Update an existing thread's mode and title
+ * - Delete a thread by ID
+ * - Get sharing information for a thread
+ */
 export const createThreadService = () => {
   return {
     /**
@@ -10,25 +20,48 @@ export const createThreadService = () => {
      * @returns an array of Thread objects
      */
     getThreads() {
-      return odeServices.http().get<Thread[]>(`${baseUrl}/threads`);
+      return odeServices.http().get<Thread[]>(`${baseUrlAPI}/threads`);
     },
+    /**
+     * Create a new thread.
+     * @param payload - The data for the new thread.
+     * @returns The created thread's ID.
+     */
     create(payload: { mode: ThreadMode; title: string }) {
       return odeServices.http().post<{
         id: ThreadId;
-      }>(`${baseUrl}/thread`, payload);
+      }>(`${baseUrlAPI}/threads`, payload);
     },
+    /**
+     * Update an existing thread.
+     * @param threadId - The ID of the thread to update.
+     * @param payload - The updated data for the thread.
+     * @returns A promise that resolves when the thread is updated.
+     */
     update(threadId: ThreadId, payload: { mode: ThreadMode; title: string }) {
       return odeServices
         .http()
-        .put<void>(`${baseUrl}/thread/${threadId}`, payload);
+        .put<void>(`${baseUrlAPI}/threads/${threadId}`, payload);
     },
+    /**
+     * Delete a thread.
+     * @param threadId - The ID of the thread to delete.
+     * @returns A promise that resolves when the thread is deleted.
+     */
     delete(threadId: ThreadId) {
-      return odeServices.http().delete<void>(`${baseUrl}/thread/${threadId}`);
+      return odeServices
+        .http()
+        .delete<void>(`${baseUrlAPI}/threads/${threadId}`);
     },
+    /**
+     * Get sharing information for a thread.
+     * @param threadId - The ID of the thread to get sharing information for.
+     * @returns A promise that resolves with the sharing information for the thread.
+     */
     getShares(threadId: ThreadId) {
       return odeServices
         .http()
-        .get<Share>(`${baseUrl}/thread/share/json/${threadId}`);
+        .get<Share>(`${baseUrlAPI}/threads/share/json/${threadId}`);
     },
   };
 };
