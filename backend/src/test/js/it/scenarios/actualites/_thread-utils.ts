7bac082e9430b945ce04288f516d25a87cc16be3
vbillard91 <vincent.billard@edifice.io>
2025-10-20 10:33:26 +0200

    feature(tests,it): #COCO-4742 add test on thread rights + refactoring on thread management

--- backend/src/test/js/it/scenarios/actualites/_thread-utils.ts
+++ backend/src/test/js/it/scenarios/actualites/_thread-utils.ts
@@ -14,6 +14,40 @@ export type Identifier = {
   id: string;
 }
 
+export enum ThreadRight {
+  CONTRIBUTOR = 'CONTRIBUTOR',
+  PUBLISHER = 'PUBLISHER',
+  MANAGER = 'MANAGER',
+}
+
+export const threadContributorRights: string[] = [ "net-atos-entng-actualites-controllers-InfoController|unsubmit",
+  "net-atos-entng-actualites-controllers-InfoController|updateDraft",
+  "net-atos-entng-actualites-controllers-InfoController|listInfosByThreadId",
+  "net-atos-entng-actualites-controllers-InfoController|shareInfoSubmit",
+  "net-atos-entng-actualites-controllers-InfoController|createPending",
+  "net-atos-entng-actualites-controllers-InfoController|createDraft",
+  "net-atos-entng-actualites-controllers-InfoController|shareInfo",
+  "net-atos-entng-actualites-controllers-InfoController|shareInfoRemove",
+  "net-atos-entng-actualites-controllers-InfoController|shareResourceInfo",
+  "net-atos-entng-actualites-controllers-InfoController|submit",
+  "net-atos-entng-actualites-controllers-ThreadController|getThread"];
+
+export const threadPublisherRights: string[] = [ "net-atos-entng-actualites-controllers-InfoController|getInfoTimeline",
+  "net-atos-entng-actualites-controllers-InfoController|createPublished",
+  "net-atos-entng-actualites-controllers-InfoController|updatePending",
+  "net-atos-entng-actualites-controllers-InfoController|unpublish",
+  "net-atos-entng-actualites-controllers-InfoController|publish",
+  "net-atos-entng-actualites-controllers-InfoController|updatePublished"];
+
+export const threadManagerRights: string[] = [ "net-atos-entng-actualites-controllers-ThreadController|shareResource",
+  "net-atos-entng-actualites-controllers-ThreadController|shareThreadSubmit",
+  "net-atos-entng-actualites-controllers-InfoController|delete",
+  "net-atos-entng-actualites-controllers-ThreadController|deleteThread",
+  "net-atos-entng-actualites-controllers-ThreadController|shareThread",
+  "net-atos-entng-actualites-controllers-ThreadController|shareThreadRemove",
+  "net-atos-entng-actualites-controllers-ThreadController|updateThread"];
+
+
 export function createThreadOrFail(title: String) : Identifier {
   let res = createThread(title);
   check(res, {
@@ -81,4 +115,43 @@ export function threadExists(id: String) : void {
   check(res, {
     "Thread should not exists ": (r) => r.status == 401,
   });
+}
+
+export enum ShareTargetType {
+  USER = 'USER',
+  GROUP = 'GROUP'
+}
+
+export function shareThreadOrFail(threadId: string, ids: string[], rights: string[], shareTargetType: ShareTargetType) {
+  const resp = shareThread(threadId, ids, rights, shareTargetType);
+  check(resp, {
+    "Thread sharing must succeed ": (r) => r.status < 300,
+  });
+}
+
+export function shareThread(threadId: string, ids: string[], rights: string[], shareTargetType: ShareTargetType) :  RefinedResponse<any> {
+
+  let body = {
+    users: {},
+    groups: {},
+    bookmark: {}
+  }
+
+  for(let i = 0; i < ids.length; i++) {
+    switch(shareTargetType){
+      case ShareTargetType.USER:
+        (body.users as any)[ids[i]] = rights;
+        break;
+      case ShareTargetType.GROUP:
+        (body.groups as any)[ids[i]] = rights;
+        break;
+    }
+  }
+
+  return http.put(`${rootUrl}/actualites/api/v1/threads/${threadId}/shares`,
+      JSON.stringify(body))
+}
+
+export function getShareThread(threadId: string) :  RefinedResponse<any> {
+  return http.get(`${rootUrl}/actualites/api/v1/threads/${threadId}/shares?search=`)
 }
