4d3b0d3d6b7ce183ead449d76407d72cc85beabc
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-11-20 18:11:14 +0100

    fix(actualites):#COCO-4360 display empty screen on a specific thread (#94)
    * fix(actualites):#COCO-4360 display empty screen on a specific thread
    
    * fix(actualites):#COCO-4360 display empty screen on a specific thread
    
    * fix tests descriptions

--- frontend/src/components/InfoList/hooks/useInfoListEmptyScreen.ts
+++ frontend/src/components/InfoList/hooks/useInfoListEmptyScreen.ts
@@ -1,3 +1,4 @@
+import { useThreadInfoParams } from '~/hooks/useThreadInfoParams';
 import { useThreadsUserRights } from '~/hooks/useThreadsUserRights';
 import { useUserRights } from '~/hooks/useUserRights';
 import { useThreads } from '~/services/queries';
@@ -10,18 +11,27 @@ export function useInfoListEmptyScreen(): {
 } {
   const { data: threads, isSuccess: isThreadsSuccess } = useThreads();
   const rights = useUserRights();
+  const { threadId } = useThreadInfoParams();
 
-  const { canContributeOnOneThread, isReady: isThreadsUserRightsReady } =
-    useThreadsUserRights();
+  const {
+    canContributeOnOneThread,
+    isReady: isThreadsUserRightsReady,
+    threadsWithContributeRight,
+  } = useThreadsUserRights();
 
   const isReady = isThreadsSuccess && isThreadsUserRightsReady;
   let type: EmptyScreenType = 'default';
 
   if (isReady) {
-    const shouldCreateThread = threads?.length === 0 && rights.canCreateThread;
-    if (shouldCreateThread) type = 'create-thread';
-    if (canContributeOnOneThread) {
-      type = 'create-info';
+    if (threads?.length === 0 && rights.canCreateThread) {
+      type = 'create-thread';
+    } else if (canContributeOnOneThread) {
+      if (
+        !threadId || // when filter on all threads (path: root/)
+        threadsWithContributeRight?.some((thread) => thread.id === threadId) // when filter on a specific thread (path: root/[threadId]/)
+      ) {
+        type = 'create-info';
+      }
     }
   }
 
