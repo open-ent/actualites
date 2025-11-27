fa7666d0710a1868ce6bec3cc73a53251022335d
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-11-27 17:58:02 +0100

    fix(actualites):#COCO-4373 fix infokey

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -20,9 +20,8 @@ const routes = (queryClient: QueryClient): RouteObject[] => [
       {
         path: 'create/info',
         async lazy() {
-          const { loader, CreateInfo: Component } = await import(
-            '~/routes/pages/CreateInfo'
-          );
+          const { loader, CreateInfo: Component } =
+            await import('~/routes/pages/CreateInfo');
           return {
             loader: loader(queryClient),
             Component,
@@ -33,9 +32,8 @@ const routes = (queryClient: QueryClient): RouteObject[] => [
             path: '',
             index: true,
             async lazy() {
-              const { loader, CreateInfoDetails: Component } = await import(
-                '~/routes/pages/CreateInfoDetails'
-              );
+              const { loader, CreateInfoDetails: Component } =
+                await import('~/routes/pages/CreateInfoDetails');
               return {
                 loader: loader(queryClient),
                 Component,
@@ -45,9 +43,8 @@ const routes = (queryClient: QueryClient): RouteObject[] => [
           {
             path: ':infoIdAsString/rights',
             async lazy() {
-              const { loader, CreateInfoRights: Component } = await import(
-                '~/routes/pages/CreateInfoRights'
-              );
+              const { loader, CreateInfoRights: Component } =
+                await import('~/routes/pages/CreateInfoRights');
               return {
                 loader: loader(queryClient),
                 Component,
@@ -57,9 +54,8 @@ const routes = (queryClient: QueryClient): RouteObject[] => [
           {
             path: ':infoIdAsString',
             async lazy() {
-              const { loader, CreateInfoDetails: Component } = await import(
-                '~/routes/pages/CreateInfoDetails'
-              );
+              const { loader, CreateInfoDetails: Component } =
+                await import('~/routes/pages/CreateInfoDetails');
               return {
                 loader: loader(queryClient),
                 Component,
@@ -71,9 +67,8 @@ const routes = (queryClient: QueryClient): RouteObject[] => [
       {
         path: '',
         async lazy() {
-          const { loader, Threads: Component } = await import(
-            '~/routes/pages/Threads'
-          );
+          const { loader, Threads: Component } =
+            await import('~/routes/pages/Threads');
           return {
             loader: loader(queryClient),
             Component,
@@ -90,9 +85,8 @@ const routes = (queryClient: QueryClient): RouteObject[] => [
                     path: '',
                     index: true,
                     async lazy() {
-                      const { loader, Threads: Component } = await import(
-                        '~/routes/pages/Threads'
-                      );
+                      const { loader, Threads: Component } =
+                        await import('~/routes/pages/Threads');
                       return {
                         loader: loader(queryClient),
                         Component,
@@ -102,9 +96,8 @@ const routes = (queryClient: QueryClient): RouteObject[] => [
                   {
                     path: 'infos/:infoIdAsString/edit',
                     async lazy() {
-                      const { loader, Threads: Component } = await import(
-                        '~/routes/pages/Threads'
-                      );
+                      const { loader, Threads: Component } =
+                        await import('~/routes/pages/Threads');
                       return {
                         loader: loader(queryClient),
                         Component,
