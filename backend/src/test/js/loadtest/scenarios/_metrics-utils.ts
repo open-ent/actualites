267fb5dd6a83da4bb154b7a56087107ae24decba
vbillard91 <vincent.billard@edifice.io>
2025-12-03 11:00:40 +0100

    feature(test): #COCO-5000 add load test for scenario T2 (#105)

--- backend/src/test/js/loadtest/scenarios/_metrics-utils.ts
+++ backend/src/test/js/loadtest/scenarios/_metrics-utils.ts
@@ -1,5 +1,16 @@
 import { Trend, Counter } from 'k6/metrics';
+import { RefinedResponse } from "k6/http";
+import { InfoUser } from "./_init-test-utils.ts";
 
 export const apiTrend = new Trend('api_time');
 export const apiErrors = new Counter('api_errors');
 export const apiSuccesses = new Counter('api_successes');
+
+export function pushResponseMetrics(response: RefinedResponse<any>, user: InfoUser) {
+  apiTrend.add(response.timings.duration, {profile: user.profile});
+  if(response.status < 300 && response.status >= 200) {
+    apiSuccesses.add(1);
+  } else {
+    apiErrors.add(1);
+  }
+}
---
+++ backend/src/test/js/loadtest/target-tests/s4-read-info.ts
@@ -0,0 +1,80 @@
+import { describe } from "https://jslib.k6.io/k6chaijs/4.3.4.0/index.js";
+
+import {
+  getHeaders,
+  switchSession,
+  Session
+} from '../../node_modules/edifice-k6-commons/dist/index.js';
+import { InitData } from "./_init-test-utils.ts";
+import { sleep, check } from "k6";
+import http from "k6/http";
+import exec from 'k6/execution';
+import { pushResponseMetrics } from "../scenarios/_metrics-utils.ts";
+import { Counter } from "k6/metrics";
+
+const rootUrl = __ENV.ROOT_URL;
+const baseDelay = (__ENV.DELAY_BETWEEN_PAGE_IN_MS ? Number(__ENV.DELAY_BETWEEN_PAGE_IN_MS) : 1000) ;
+
+const totalUser = new Counter("total_users_s4");
+
+export function s4ReadInfo(data: InitData) {
+
+  describe('[s4-Read-Info] Test scenario s4 access to actualites read an info', () => {
+
+    const randomIndex = exec.scenario.iterationInInstance % data.allSessions.length;
+
+    const user = data.allSessions[randomIndex];
+    const session = Session.from(user.session);
+    switchSession(session);
+    totalUser.add(1);
+
+    const widgetUrl = `${rootUrl}/actualites/api/v1/infos/preview/last/4`;
+    const res = http.get(widgetUrl,
+      { headers: getHeaders(), tags:{ type: 'widget_access'} });
+    //handle metrics
+    pushResponseMetrics(res, user);
+
+    sleep(baseDelay / 1000);
+
+    const infos = JSON.parse(res.body as string);
+    if(!infos || infos.length === 0) {
+      console.error('user in dataset not correct', user);
+      return;
+    }
+    const infoId = infos[0].id;
+
+    //display interface actualites
+    const listInfoUrl = `${rootUrl}/actualites/api/v1/infos`;
+    const resListInfo = http.get(listInfoUrl,
+      { headers: getHeaders(), tags: {type : 'list_info'} });
+    pushResponseMetrics(resListInfo, user);
+    // short delay to simulate succession of call from browser
+    sleep(baseDelay / 5000);
+    const infosList = JSON.parse(res.body as string);
+
+    check(infosList, {
+      "Infos list should not be empty": (list) => Array.isArray(list) && list.length !== 0,
+    });
+
+    //display thread list
+    const listThreadUrl = `${rootUrl}/actualites/api/v1/threads`;
+    const resListThread = http.get(listThreadUrl,
+      { headers: getHeaders(), tags: {type : 'list_thread'} });
+    pushResponseMetrics(resListThread, user);
+    sleep(baseDelay / 5000);
+
+    //display stats
+    const statsUrl = `${rootUrl}/actualites/api/v1/infos/stats`;
+    const resStats = http.get(statsUrl, { headers: getHeaders(),
+      tags: {type : 'stats'}});
+    pushResponseMetrics(resStats, user);
+    sleep(baseDelay / 5000);
+
+    //display info
+    const infoUrl = `${rootUrl}/actualites/api/v1/infos/${infoId}`;
+    const resInfo = http.get(infoUrl, { headers: getHeaders(),
+      tags: {type : 'get_info'} });
+    pushResponseMetrics(resInfo, user);
+    sleep(baseDelay / 1000);
+  });
+}
