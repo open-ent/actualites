dbea303faeab389350c538439f44dfba5aa2409e
Vincent Billard <vincent.billard@edifice.io>
2025-12-05 11:12:48 +0100

    fix multi adml thread creation

--- backend/src/main/java/net/atos/entng/actualites/controllers/v1/ThreadControllerV1.java
+++ backend/src/main/java/net/atos/entng/actualites/controllers/v1/ThreadControllerV1.java
@@ -104,30 +104,24 @@ public class ThreadControllerV1 extends ControllerHelper {
     @ResourceFilter(ThreadFilter.class)
     @SecuredAction(value = "actualites.create", right = ROOT_RIGHT + "|createThread")
     public void createThread(final HttpServerRequest request) {
-		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
-			@Override
-			public void handle(final UserInfos user) {
-				RequestUtils.bodyToJson(request, pathPrefix + SCHEMA_THREAD_CREATE, resource -> {
-                    // WB-1402 auto-attach the thread to this user's structure, iif only one exists.
-                    final List<String> structures = user.getStructures();
-                    if(structures!=null && structures.size() == 1) {
-                        String structureId = structures.get(0);
-                        if(structureId!=null && structureId.length()>0) {
-                            resource.put("structureId", structureId);
-                        }
-                    }
-                    final Handler<Either<String,JsonObject>> handler = notEmptyResponseHandler(request);
-                    crudService.create(resource, user, h -> {
-                        if(h.isRight()) {
-                            Promise<Void> promise = Promise.promise();
-                            threadMigrationService.addAdmlShare(h.right().getValue().getString("id"), promise);
-                            promise.future().onSuccess( migr ->
-                                eventHelper.onCreateResource(request, RESOURCE_NAME, handler).handle(h));
-                        }
-                    });
-                });
-			}
-		});
+		UserUtils.getUserInfos(eb, request,user -> RequestUtils.bodyToJson(request, pathPrefix + SCHEMA_THREAD_CREATE,
+				resource -> {
+					// WB-1402 auto-attach the thread to this user's structure, iif only one exists.
+					final List<String> structures = user.getStructures();
+					if(structures!=null && structures.size() == 1) {
+						String structureId = structures.get(0);
+						if(structureId!=null && structureId.length()>0) {
+							resource.put("structureId", structureId);
+						}
+					}
+					final Handler<Either<String,JsonObject>> handler = notEmptyResponseHandler(request);
+					crudService.create(resource, user, h -> {
+						if(h.isRight()) {
+							threadMigrationService.addAdmlShare(h.right().getValue().getString("id"));
+							eventHelper.onCreateResource(request, RESOURCE_NAME, handler).handle(h);
+						}
+					});
+				}));
 	}
 
     @Put("/api/v1/threads/:" + THREAD_RESOURCE_ID)
@@ -136,17 +130,8 @@ public class ThreadControllerV1 extends ControllerHelper {
     @SecuredAction(value = "thread.manager", right = ROOT_RIGHT + "|updateThread", type = ActionType.RESOURCE)
     public void updateThread(final HttpServerRequest request) {
 		final String threadId = request.params().get(Actualites.THREAD_RESOURCE_ID);
-		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
-			@Override
-			public void handle(final UserInfos user) {
-				RequestUtils.bodyToJson(request, pathPrefix + SCHEMA_THREAD_UPDATE, new Handler<JsonObject>() {
-					@Override
-					public void handle(JsonObject resource) {
-						crudService.update(threadId, resource, user, notEmptyResponseHandler(request));
-					}
-				});
-			}
-		});
+		UserUtils.getUserInfos(eb, request, user -> RequestUtils.bodyToJson(request, pathPrefix + SCHEMA_THREAD_UPDATE,
+				resource -> crudService.update(threadId, resource, user, notEmptyResponseHandler(request))));
 	}
 
     @Delete("/api/v1/threads/:" + THREAD_RESOURCE_ID)
@@ -155,12 +140,7 @@ public class ThreadControllerV1 extends ControllerHelper {
     @SecuredAction(value = "thread.manager", right = ROOT_RIGHT + "|deleteThread", type = ActionType.RESOURCE)
     public void deleteThread(final HttpServerRequest request) {
 		final String threadId = request.params().get(Actualites.THREAD_RESOURCE_ID);
-		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
-			@Override
-			public void handle(final UserInfos user) {
-				crudService.delete(threadId, user, notEmptyResponseHandler(request));
-			}
-		});
+		UserUtils.getUserInfos(eb, request, user -> crudService.delete(threadId, user, notEmptyResponseHandler(request)));
 	}
 
     @Get("/api/v1/threads/:id/shares")
@@ -173,52 +153,46 @@ public class ThreadControllerV1 extends ControllerHelper {
 			badRequest(request);
 			return;
 		}
-		getUserInfos(eb, request, new Handler<UserInfos>() {
-			@Override
-			public void handle(final UserInfos user) {
-				if (user != null) {
-					shareService.shareInfos(user.getUserId(), id, I18n.acceptLanguage(request), request.params().get("search"), new Handler<Either<String, JsonObject>>() {
-						@Override
-						public void handle(Either<String, JsonObject> event) {
-							final Handler<Either<String, JsonObject>> handler = defaultResponseHandler(request);
-							if(event.isRight()){
-								JsonObject result = event.right().getValue();
-								if(result.containsKey("actions")){
-									JsonArray actions = result.getJsonArray("actions");
-									JsonArray newActions = new JsonArray();
-									for(Object action : actions){
-										if(((JsonObject) action).containsKey("displayName")){
-											String displayName = ((JsonObject) action).getString("displayName");
-											if(displayName.contains(".")){
-												String resource = displayName.split("\\.")[0];
-												if(resource.equals(RESOURCE_NAME)){
-													newActions.add(action);
-												}
-											}
-										}
-									}
-									result.put("actions", newActions);
-								}
-								threadService.getOwnerInfo(id, h -> {
-									if(h.isRight()) {
-										result.put("owner", h.right().getValue().getString("owner"));
-										addNormalizedRights(result);
-										handler.handle(new Either.Right<String, JsonObject>(result));
-									} else {
-										handler.handle(new Either.Left<String, JsonObject>("Error finding owner of the resource."));
-									}
-								});
-							} else {
-								handler.handle(new Either.Left<String, JsonObject>("Error finding shared resource."));
-							}
-						}
-					});
+		getUserInfos(eb, request, user -> {
+            if (user != null) {
+                shareService.shareInfos(user.getUserId(), id, I18n.acceptLanguage(request), request.params().get("search"), event -> {
+                    final Handler<Either<String, JsonObject>> handler = defaultResponseHandler(request);
+                    if(event.isRight()){
+                        JsonObject result = event.right().getValue();
+                        if(result.containsKey("actions")){
+                            JsonArray actions = result.getJsonArray("actions");
+                            JsonArray newActions = new JsonArray();
+                            for(Object action : actions){
+                                if(((JsonObject) action).containsKey("displayName")){
+                                    String displayName = ((JsonObject) action).getString("displayName");
+                                    if(displayName.contains(".")){
+                                        String resource = displayName.split("\\.")[0];
+                                        if(resource.equals(RESOURCE_NAME)){
+                                            newActions.add(action);
+                                        }
+                                    }
+                                }
+                            }
+                            result.put("actions", newActions);
+                        }
+                        threadService.getOwnerInfo(id, h -> {
+                            if(h.isRight()) {
+                                result.put("owner", h.right().getValue().getString("owner"));
+                                addNormalizedRights(result);
+                                handler.handle(new Either.Right<String, JsonObject>(result));
+                            } else {
+                                handler.handle(new Either.Left<String, JsonObject>("Error finding owner of the resource."));
+                            }
+                        });
+                    } else {
+                        handler.handle(new Either.Left<String, JsonObject>("Error finding shared resource."));
+                    }
+                });
 
-				} else {
-					unauthorized(request);
-				}
-			}
-		});
+            } else {
+                unauthorized(request);
+            }
+        });
 	}
 
     @Put("/api/v1/threads/:id/shares")
@@ -226,27 +200,24 @@ public class ThreadControllerV1 extends ControllerHelper {
     @ResourceFilter(ThreadFilter.class)
     @SecuredAction(value = "thread.manager", right = ROOT_RIGHT + "|shareResource", type = ActionType.RESOURCE)
     public void updateThreadShares(final HttpServerRequest request) {
-		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
-			@Override
-			public void handle(final UserInfos user) {
-				if (user != null) {
-					final String id = request.params().get("id");
-					if(id == null || id.trim().isEmpty()) {
-						badRequest(request, "invalid.id");
-						return;
-					}
+		UserUtils.getUserInfos(eb, request, user -> {
+            if (user != null) {
+                final String id = request.params().get("id");
+                if(id == null || id.trim().isEmpty()) {
+                    badRequest(request, "invalid.id");
+                    return;
+                }
 
-					JsonObject params = new JsonObject()
-							.put("profilUri", "/userbook/annuaire#" + user.getUserId() + "#" + user.getType())
-							.put("username", user.getUsername())
-							.put("resourceUri", pathPrefix + "#/default");
+                JsonObject params = new JsonObject()
+                        .put("profilUri", "/userbook/annuaire#" + user.getUserId() + "#" + user.getType())
+                        .put("username", user.getUsername())
+                        .put("resourceUri", pathPrefix + "#/default");
 
-					shareResource(request, "news.thread-shared", false, params, "title");
-				} else {
-					unauthorized(request);
-				}
-			}
-		});
+                shareResource(request, "news.thread-shared", false, params, "title");
+            } else {
+                unauthorized(request);
+            }
+        });
 	}
 
     @Post("/api/v1/threads/tasks")
