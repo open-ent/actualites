1e567de3f3cd46712df3c5890844b7a0cf866e07
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2025-12-08 09:23:59 +0100

    fix: #COCO-4410, corrected comment length, publish date and unit test

--- frontend/src/hooks/useInfoUserRights.ts
+++ frontend/src/hooks/useInfoUserRights.ts
@@ -1,8 +1,8 @@
-import { useUser } from '@edifice.io/react';
+import { useEdificeClient } from '@edifice.io/react';
 import { Info } from '~/models/info';
 
 export function useInfoUserRights(info: Info) {
-  const { user } = useUser();
+  const { user } = useEdificeClient();
   const isCreator = user?.userId === info.owner.id;
   const canComment =
     info.sharedRights.findIndex((value) => value === 'info.comment') >= 0;
