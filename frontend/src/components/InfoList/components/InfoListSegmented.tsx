13d3e87008278ef24954bed5bf9deaf77c512b82
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-12-09 14:45:59 +0100

    fix(actualites):#COCO-4373 replace Segmented by composant from fronte… (#112)
    * fix(actualites):#COCO-4373 replace Segmented by composant from frontend-framework
    
    * add skeleton
    
    * clean div
    
    * fixes
    
    * remove react style

--- frontend/src/components/InfoList/components/InfoListSegmented.tsx
+++ frontend/src/components/InfoList/components/InfoListSegmented.tsx
@@ -1,4 +1,4 @@
-import { Segmented } from 'antd';
+import { SegmentedControl } from '@edifice.io/react';
 import { useInfoStats } from '~/components/InfoList/hooks/useInfoStats';
 import { useI18n } from '~/hooks/useI18n';
 import { useThreadInfoParams } from '~/hooks/useThreadInfoParams';
@@ -45,10 +45,10 @@ export const InfoListSegmented = ({
   ];
 
   return (
-    <Segmented
+    <SegmentedControl
       options={options}
       value={value}
-      onChange={onChange}
+      onChange={(value) => onChange(value as InfoSegmentedValue)}
       data-testid="info-list-segmented"
     />
   );
