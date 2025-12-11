230e56ad7d0bb80e7addc37a41869286c735f47f
jcbe-ode <67910608+jcbe-ode@users.noreply.github.com>
2025-12-11 16:56:03 +0100

    feat: #COCO-4593, manage links to comments (#115)
    * chore: update mockServiceWorker
    
    * feat: #COCO-4593, manage links to comments
    
    * code refactored
    
    * move hash management in a single route file
    
    * update pnpm-lock.yaml
    
    * feedback from copilot
    
    * feedback from copilot
    
    * reword comment
    
    * refactor redirection
    
    * code simplification
    
    * fix nbOfComments bug

--- frontend/src/components/comment-list/CommentList.tsx
+++ frontend/src/components/comment-list/CommentList.tsx
@@ -2,15 +2,25 @@ import { CommentProvider } from '@edifice.io/react/comments';
 import { useCommentList } from '~/hooks/useCommentList';
 import { Info } from '~/models/info';
 
+const MAX_COMMENTS_WITHOUT_PAGINATION = 100;
+
 export type CommentListProps = {
   /**
    * Comments to display in the card.
    */
   info: Info;
+  withPagination?: boolean;
 };
 
-export const CommentList = ({ info }: CommentListProps) => {
+export const CommentList = ({
+  info,
+  withPagination = true,
+}: CommentListProps) => {
   const { comments, type, callbacks, options, rights } = useCommentList(info);
+
+  if (!withPagination)
+    options.maxComments = options.additionalComments =
+      MAX_COMMENTS_WITHOUT_PAGINATION;
 
   return (
     comments && (
--- frontend/src/routes/root/index.tsx
+++ frontend/src/routes/root/index.tsx
@@ -9,9 +9,7 @@ import {
 } from '@edifice.io/react';
 
 import {
-  matchPath,
   Outlet,
-  replace,
   useLoaderData,
   useLocation,
   useNavigate,
@@ -26,55 +24,12 @@ import { useThreadsUserRights } from '~/hooks/useThreadsUserRights';
 import { queryClient } from '~/providers';
 import { actionsQueryOptions } from '~/services/queries/actions';
 import { useActionUserRights } from '~/store';
-import { basename } from '..';
 
 /** Check old format URL and redirect if needed */
 export const loader = async () => {
   const actionUserRights = await queryClient.ensureQueryData(
     actionsQueryOptions(existingActions),
   );
-  const hashLocation = location.hash.substring(1);
-  // Check if the URL is an old format (angular root with hash) and redirect to the new format
-  let redirectPath;
-  if (hashLocation) {
-    let shouldReinterpretRoute = false;
-    const hasDefaultFilter = matchPath('/default?filter=:filter', hashLocation);
-
-    if (hasDefaultFilter) {
-      redirectPath = `/threads/${hasDefaultFilter?.params.filter}`;
-    } else {
-      const isDefault = matchPath('/default', hashLocation);
-
-      if (isDefault) {
-        // Suppress unused hash but do not reload the page
-        redirectPath = `/threads`;
-      } else {
-        const isPathWithInfo = matchPath(
-          '/view/thread/:threadId/info/:infoId',
-          hashLocation,
-        );
-        const isPathWithThread = matchPath('/view/thread/:id', hashLocation);
-
-        if (isPathWithInfo) {
-          // Redirect to the new format
-          redirectPath = `/threads/${isPathWithInfo.params.threadId}#info-${isPathWithInfo.params.infoId}`;
-          shouldReinterpretRoute = true; // Replace in history AND let the router reinterpret query params
-        } else if (isPathWithThread) {
-          // Redirect to the new format
-          redirectPath = `/threads/${isPathWithThread.params.id}`;
-          shouldReinterpretRoute = true; // Replace in history AND let the router reinterpret query params
-        }
-      }
-    }
-
-    if (redirectPath) {
-      const newUrl =
-        window.location.origin + basename.replace(/\/$/g, '') + redirectPath;
-      window.history.replaceState(null, '', newUrl);
-      if (shouldReinterpretRoute) return replace(newUrl);
-    }
-  }
-
   return { actionUserRights };
 };
 
@@ -90,20 +45,21 @@ export const Root = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { canContributeOnOneThread } = useThreadsUserRights();
+
+  if (!init) return <LoadingScreen position={false} />;
+
   const displayApp = {
     displayName: 'news',
     icon: 'actualites-large',
   };
-  const isAdminThreadPath = window.location.pathname.includes('/admin/threads');
-
   const pathname = location.pathname;
+  const isAdminThreadPath = pathname.includes('/admin/threads');
   const isCreateRoute = pathname.includes('/create/info');
 
   const handleClickNewInfo = () => {
     navigate('/create/info');
   };
 
-  if (!init) return <LoadingScreen position={false} />;
   return init ? (
     <Layout>
       <AppHeader>
