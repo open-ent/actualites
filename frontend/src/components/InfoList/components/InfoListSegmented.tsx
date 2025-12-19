a4db706a4b00a68f629180a63979577872967002
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-12-19 11:32:19 +0100

    feat(actualites):#COCO-4374 display pending infos (#130)

--- frontend/src/components/InfoList/components/InfoListSegmented.tsx
+++ frontend/src/components/InfoList/components/InfoListSegmented.tsx
@@ -2,6 +2,7 @@ import { SegmentedControl } from '@edifice.io/react';
 import { useInfoStats } from '~/components/InfoList/hooks/useInfoStats';
 import { useI18n } from '~/hooks/useI18n';
 import { useThreadInfoParams } from '~/hooks/useThreadInfoParams';
+import { useThreadsUserRights } from '~/hooks/useThreadsUserRights';
 import {
   InfoExtendedStatus,
   InfoSegmentedValue,
@@ -24,19 +25,34 @@ export const InfoListSegmented = ({
   const { t } = useI18n();
   const { threadId } = useThreadInfoParams();
   const { data: infosStats } = useInfosStats();
-
   const threadInfosStats = useInfoStats(infosStats, threadId);
+  const { threadsWithManageRight, canManageOnOneThread } =
+    useThreadsUserRights();
+  const canManageCurrentThread =
+    (!threadId && canManageOnOneThread) ||
+    !!(
+      threadId &&
+      threadsWithManageRight?.some((thread) => thread.id === threadId)
+    );
 
   const options: InfoListSegmentedOption[] = [
     {
       label: `${t('actualites.infoList.segmented.published')} ${threadInfosStats.status[InfoStatus.PUBLISHED]}`,
       value: InfoStatus.PUBLISHED,
     },
-    {
-      label: `${t('actualites.infoList.segmented.draft')} ${threadInfosStats.status[InfoStatus.DRAFT]}`,
-      value: InfoStatus.DRAFT,
-    },
   ];
+
+  if (canManageCurrentThread) {
+    options.push({
+      label: `${t('actualites.infoList.segmented.pending')} ${threadInfosStats.status[InfoStatus.PENDING]}`,
+      value: InfoStatus.PENDING,
+    });
+  }
+
+  options.push({
+    label: `${t('actualites.infoList.segmented.draft')} ${threadInfosStats.status[InfoStatus.DRAFT]}`,
+    value: InfoStatus.DRAFT,
+  });
 
   if (threadInfosStats.expiredCount > 0) {
     options.push({
