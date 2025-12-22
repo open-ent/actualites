7c52d30fb07ce5609e5dead8cc566fe6dd35fd7a
Vincent Billard <vincent.billard@edifice.io>
2025-12-22 13:56:50 +0100

    fix regex in display controller

--- backend/src/main/java/net/atos/entng/actualites/controllers/DisplayController.java
+++ backend/src/main/java/net/atos/entng/actualites/controllers/DisplayController.java
@@ -82,7 +82,7 @@ public class DisplayController extends BaseController {
 	 * Query parameters (?status=...) and hash fragments (#infos-..., #comments-...)
 	 * are handled by the frontend router and not part of this regex.
 	 */
-	@Get(value = "(?:/?(?:threads/[^/\\\\s]+|infos/[^/\\\\s]+(?:/edit)?|create/info(?:/[^/\\\\s]+(?:/rights)?)?|print/[^/\\\\s]+|admin/threads/?)?)?", regex = true)
+	@Get(value = "(?:/?(?:threads/[0-9]+|infos/[^/\\\\s]+(?:/edit)?|create/info(?:/[^/\\\\s]+(?:/rights)?)?|print/[^/\\\\s]+|admin/threads/?)?)?", regex = true)
 	@SecuredAction("actualites.view")
 	public void view(final HttpServerRequest request) {
 		renderView(request, new JsonObject(), "index.html", null);
