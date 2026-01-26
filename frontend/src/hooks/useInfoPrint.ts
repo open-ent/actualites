472121039b69ea4922329b73eec856ac11bea281
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-01-26 10:51:20 +0100

    fix: #COCO-4401, apply baseUrl prefix for printing

--- frontend/src/hooks/useInfoPrint.ts
+++ frontend/src/hooks/useInfoPrint.ts
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { Info } from '~/models/info';
+import { baseUrl } from '~/services';
 
 export default function useInfoPrint() {
   const [isPrintAlertOpen, setPrintAlertOpen] = useState(false);
 
   const print = (info: Info, withComments: boolean) => {
     window.open(
-      `/infos/${info.id}/print${withComments ? '?withComments=true' : ''}`,
+      `${baseUrl}/infos/${info.id}/print${withComments ? '?withComments=true' : ''}`,
       '_blank',
     );
   };
